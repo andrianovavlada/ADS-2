d16df62896cc922fde938ac4c0c9f7c55d1b9dc7
Andrianova Vlada <99177026+andrianovavlada@users.noreply.github.com>
2022-02-27 12:28:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,7 @@
 
 
 double pown(double value, uint16_t n) {
-    uint64_t a = value;
+    double a = value;
     while (n != 1) {
         value = value * a;
         n--;
@@ -20,15 +20,15 @@ uint64_t fact(uint16_t n) {
 }
 
 double calcItem(double x, uint16_t n) {
-    float y = 0;
+    double y = 0;
     y = pown(x, n) / fact(n);
     return y;
 }
 
 double expn(double x, uint16_t count) {
-    float y = 0;
-    while (count!=0){
-        y = y + calcItem(x, count) ;
+    double y = 0;
+    while (count != 0) {
+        y = y + calcItem(x, count);
         count--;
     }
     y += 1;
@@ -36,30 +36,21 @@ double expn(double x, uint16_t count) {
 }
 
 double sinn(double x, uint16_t count) {
-    float y = 0;
-    uint16_t s = count - 1;
-    uint16_t w = -1;
-    uint16_t a = 2*count - 1;
-    while (count != 1) { 
-        y = y + pown(w, s) * calcItem(x, a);
+    double y = 0;
+
+    while (count != 1) {
+        y = y + pown(-1, count - 1) * calcItem(x, 2 * count - 1);
         count--;
-        s = count - 1;
-        a = 2 * count - 1;
     }
     y += x;
     return y;
 }
 
 double cosn(double x, uint16_t count) {
-    float y = 0;
-    uint16_t s = count - 1;
-    uint16_t w = -1;
-    uint16_t a = 2 * count - 2;
+    double y = 0;
     while (count != 1) {
-        y = y + pown(w, s) * calcItem(x, a);
-        count--;
-        s = count - 1;
-        a = 2 * count - 2;
+        y = y + pown(-1, count - 1) * calcItem(x, 2 * count - 2);
+        count--;   
     }
     y += 1;
     return y;
