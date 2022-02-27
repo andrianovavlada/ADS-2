1d39c36bf14724ff5af88a38e5ba738d87ad3a1c
Andrianova Vlada <99177026+andrianovavlada@users.noreply.github.com>
2022-02-27 11:23:09 +0300

    Andrianova IST-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,63 @@
 
 
 double pown(double value, uint16_t n) {
-
+    uint64_t a = value;
+    while (n != 1) {
+        value = value * a;
+        n--;
+    }
+    return value;
 }
 
 uint64_t fact(uint16_t n) {
-
+    if (n == 1)
+        return 1;
+    else
+        return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-
+    float y = 0;
+    y = pown(x, n) / fact(n);
+    return y;
 }
 
 double expn(double x, uint16_t count) {
-
+    float y = 0;
+    while (count!=0){
+        y = y + calcItem(x, count) ;
+        count--;
+    }
+    y += 1;
+    return y;
 }
 
 double sinn(double x, uint16_t count) {
-
+    float y = 0;
+    uint16_t s = count - 1;
+    uint16_t w = -1;
+    uint16_t a = 2*count - 1;
+    while (count != 1) { 
+        y = y + pown(w, s) * calcItem(x, a);
+        count--;
+        s = count - 1;
+        a = 2 * count - 1;
+    }
+    y += x;
+    return y;
 }
 
 double cosn(double x, uint16_t count) {
-
+    float y = 0;
+    uint16_t s = count - 1;
+    uint16_t w = -1;
+    uint16_t a = 2 * count - 2;
+    while (count != 1) {
+        y = y + pown(w, s) * calcItem(x, a);
+        count--;
+        s = count - 1;
+        a = 2 * count - 2;
+    }
+    y += 1;
+    return y;
 }
